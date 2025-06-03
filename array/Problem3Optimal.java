5cba38885864abceaac584b5a9b52ff2e3196e2f
John Doe <bhattsonali36@gmail.com>
2025-06-04 01:00:39 +0530

    Array Problem 3 (Optimal solution)

---
+++ array/Problem3Optimal.java
@@ -0,0 +1,34 @@
+package array;
+
+public class Problem3Optimal {
+    static int[] LocatingZerosOptimalSolution(int[] arr, int n){
+        int j = -1;
+        for(int i=0; i<n; i++){
+            if(arr[i] == 0){
+                j = i;
+                break;
+            }
+        }
+
+        if(j == -1) return arr;
+        int temp;
+        for(int i=j+1; i<n;i++){
+            if(arr[i] != 0){
+                temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                j++;
+            }
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        int[] arr = {1, 0, 2, 3, 4, 0, 0, 5, 1, 6}; 
+        int n = arr.length;
+        LocatingZerosOptimalSolution(arr, n);
+        for (int i = 0; i < n; i++) {
+            System.err.println(arr[i]);
+        }
+    }
+    
+}
